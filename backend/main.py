d1b927b577162e0fe008b6417d50914776a72457
Petit-DJ <itssimplydjsuryansh@gmail.com>
2026-07-15 18:13:56 +0530

    added web socket

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI, APIRouter, Request
+from fastapi import FastAPI, APIRouter, Request, Response
 from routerr import blog_post
 from db import models
+
 from db.database import engine
-from routerr import user, article, product
+from routerr import user, article, product, file
 import os
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from auth import authentication
+from client import html
+from fastapi.websockets import WebSocket
 
 rakesh = FastAPI()
 # rakesh.include_router(house_get.router)
 rakesh.include_router(blog_post.router)
+rakesh.include_router(file.router)
 rakesh.include_router(user.router)
 rakesh.include_router(article.router)
 rakesh.include_router(product.router)
@@ -22,6 +26,22 @@ def hello():
     return {"message": "Hello, World!"}
 
 origins = ['http://localhost:3000' ]
+
+@rakesh.get("/")
+async def get():
+    return HTMLResponse(html)
+
+clients =[]
+
+@rakesh.websocket("/chat")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    while True:
+       data = await websocket.receive_text()
+       for client in clients:
+          await client.send_text(data)
+    
 
 rakesh.add_middleware(
     CORSMiddleware,
---
+++ backend/routerr/file.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, File , UploadFile
+import shutil
+
+
+router = APIRouter (
+    prefix='/file',
+    tags = ['file']
+)
+
+
+@router.post('/file')
+def get_file(file: bytes = File(...)):
+    content = file.decode('utf-8')
+    lines = content.split('\n')
+    return {'lines': lines}
+
+@router.post('/file/upload_file')
+def upload_file(file: UploadFile = File (...)):
+    path =  f"files/{file.filename}"
+    with open(path, 'w+b') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return{
+        'filename': path,
+        'type': file.content_type
+    }
